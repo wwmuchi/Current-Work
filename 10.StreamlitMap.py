189dba6d1cb183280ca0f11f9eb5d7126f085b60
Will Moller <wmoller@uchicago.edu>
2024-12-29 15:42:11 -0400

    checking 2

--- 10.StreamlitMap.py
+++ 10.StreamlitMap.py
@@ -131,6 +131,7 @@ map_name = f"{geography_data_type}_{exposure_data_type}_{hh_definition_joined}_{
 DROPBOX_ACCESS_TOKEN = st.secrets["dropbox_key"]
 st.write(DROPBOX_ACCESS_TOKEN)
 st.stop()
+
 dbx = dropbox.Dropbox(DROPBOX_ACCESS_TOKEN)
 
 # Corrected folder path in Dropbox
